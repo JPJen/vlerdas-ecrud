dc5c264ad4a2d55066c3500eed5eb62ac7ada79b
moroni-pickering <m@itsolver.com>
2013-11-11 11:33:56 -0700

    Tests to verify base64 decode works
    1MB test is failing :S

--- test/libtest.js
+++ test/libtest.js
@@ -7,60 +7,56 @@ var xotree = new UTIL.XML.ObjTree();
 var Jsonpath = require('JSONPath');
 
 module.exports = exports = function(request) {
-    return {
-	checkDELETE_GridFSDoc : function(gridFSDocId, httpCode) {
-	    describe('DELETE /ecrud/v1/core/fs/' + gridFSDocId, function() {
-		it('respond with json', function(done) {
-		    request.del('/ecrud/v1/core/fs/' + gridFSDocId).expect(httpCode, done);
-		});
-	    });
-	},
-	checkGET_GridFSDoc : function(gridFSDocId, httpCode) {
-	    describe('GET /ecrud/v1/core/fs/' + gridFSDocId, function() {
-		it('respond with json', function(done) {
-		    request.get('/ecrud/v1/core/fs/' + gridFSDocId).expect(httpCode, done);
-		});
-	    });
-	},
-	checkGET_Collection : function(collectionName, collectionId, httpCode) {
-	    describe('GET /ecrud/v1/core/' + collectionName + '/' + collectionId, function() {
-		it('respond with json', function(done) {
-		    request.get('/ecrud/v1/core/' + collectionName + '/' + collectionId)
-		    // .set('Accept', 'application/json')
-		    // .expect('Content-Type', /json/)
-		    .expect(httpCode, done);
-		});
-	    });
-	},
-	checkDELETE_Collection : function(collectionName, collectionId, httpCode) {
-	    describe('DELETE /ecrud/v1/core/' + collectionName + '/' + collectionId, function() {
-		it('respond with json', function(done) {
-		    request.del('/ecrud/v1/core/' + collectionName + '/' + collectionId).expect(httpCode, done);
-		});
-	    });
-	},
-	checkXmlDocIds : function(collectionName, collectionId, json) {
-	    describe('GET /ecrud/v1/core/' + collectionName + '/' + collectionId, function() {
-		it('XML check ids', function(done) {
-		    request.get('/ecrud/v1/core/' + collectionName + '/' + collectionId).set('Accept',
-			    'application/xml').end(
-			    function(err, res) {
-				var jsonFromXML = xotree.parseXML(res.text);
-				var attachmentsFromXML = Jsonpath.eval(jsonFromXML, '$..nc:Attachment');
-				var attachments = Jsonpath.eval(json, '$..nc:Attachment');
-				attachmentsFromXML[0]['nc:BinaryLocationURI'].should
-					.equal(attachments[0]['nc:BinaryLocationURI']);
+	return {
+		checkDELETE_GridFSDoc : function(gridFSDocId, httpCode, desc) {
+			describe(desc + 'DELETE /ecrud/v1/core/fs/' + gridFSDocId, function() {
+				it('respond with json', function(done) {
+					request.del('/ecrud/v1/core/fs/' + gridFSDocId).expect(httpCode, done);
+				});
+			});
+		},
+		checkGET_GridFSDoc : function(gridFSDocId, httpCode, desc) {
+			describe(desc + 'GET /ecrud/v1/core/fs/' + gridFSDocId, function() {
+				it('respond with json', function(done) {
+					request.get('/ecrud/v1/core/fs/' + gridFSDocId).expect(httpCode, done);
+				});
+			});
+		},
+		checkGET_Collection : function(collectionName, collectionId, httpCode, desc) {
+			describe(desc + 'GET /ecrud/v1/core/' + collectionName + '/' + collectionId, function() {
+				it('respond with json', function(done) {
+					request.get('/ecrud/v1/core/' + collectionName + '/' + collectionId)
+					// .set('Accept', 'application/json')
+					// .expect('Content-Type', /json/)
+					.expect(httpCode, done);
+				});
+			});
+		},
+		checkDELETE_Collection : function(collectionName, collectionId, httpCode, desc) {
+			describe(desc + 'DELETE /ecrud/v1/core/' + collectionName + '/' + collectionId, function() {
+				it('respond with json', function(done) {
+					request.del('/ecrud/v1/core/' + collectionName + '/' + collectionId).expect(httpCode, done);
+				});
+			});
+		},
+		checkXmlDocIds : function(collectionName, collectionId, json, desc) {
+			describe(desc + 'GET /ecrud/v1/core/' + collectionName + '/' + collectionId, function() {
+				it('XML check ids', function(done) {
+					request.get('/ecrud/v1/core/' + collectionName + '/' + collectionId).set('Accept', 'application/xml').end(function(err, res) {
+						var jsonFromXML = xotree.parseXML(res.text);
+						var attachmentsFromXML = Jsonpath.eval(jsonFromXML, '$..nc:Attachment');
+						var attachments = Jsonpath.eval(json, '$..nc:Attachment');
+						attachmentsFromXML[0]['nc:BinaryLocationURI'].should.equal(attachments[0]['nc:BinaryLocationURI']);
 
-				var docFromXML = Jsonpath.eval(jsonFromXML, '$..nc:Document');
-				var doc = Jsonpath.eval(json, '$..nc:Document');
-				docFromXML[0]['nc:DocumentFileControlID'].should
-					.equal(doc[0]['nc:DocumentFileControlID']);
+						var docFromXML = Jsonpath.eval(jsonFromXML, '$..nc:Document');
+						var doc = Jsonpath.eval(json, '$..nc:Document');
+						docFromXML[0]['nc:DocumentFileControlID'].should.equal(doc[0]['nc:DocumentFileControlID']);
 
-				collectionId.should.equal(jsonFromXML.document._id);
-				done();
-			    });
-		});
-	    });
-	}
-    };
+						collectionId.should.equal(jsonFromXML.document._id);
+						done();
+					});
+				});
+			});
+		}
+	};
 };
