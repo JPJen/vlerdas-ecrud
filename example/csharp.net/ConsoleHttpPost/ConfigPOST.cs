d2d413e9edb7e3222b78320a1c6d458006a980b9
moroni-pickering <m@itsolver.com>
2014-01-24 14:29:20 -0700

    .Net console app enhancements

---
+++ example/csharp.net/ConsoleHttpPost/ConfigPOST.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace ConsoleHttpPost {
+    class ConfigPOST {
+
+        public string Base64FileName { get; set; }
+
+        internal static ConfigPOST getConfigPOST() {
+            var config = new ConfigPOST();
+            //check for config file, load if exsists, 
+            //if not load defaults
+            
+            //string loadFileName = @"E:\VA\VLERDoc.xml";
+            //string loadFileName = @"E:\Code\GitHub\vlerdas-ecrud\test\attachments\eCFT1MBAttachEmbeded.xml";
+            //string loadFileName = @"C:\vler-proto-out\generated_out\xmlWithEmbeddedB64_5MB_a.xml"; //6.8 MB
+            //string loadFileName = @"C:\vler-proto-out\generated_out\xmlWithEmbeddedB64_14MB_a.xml"; //19.1 MB
+            //string loadFileName = @"C:\vler-proto-out\generated_out\xmlWithEmbeddedB64_17MB_a.xml"; //23.2 MB
+            //string loadFileName = @"C:\vler-proto-out\generated_out\xmlWithEmbeddedB64_21MB_a.xml"; //28.7 MB
+            //string loadFileName = @"C:\vler-proto-out\generated_out\xmlWithEmbeddedB64_25MB_a.xml"; //34.2 MB
+            //string loadFileName = @"C:\vler-proto-out\generated_out\xmlAttachment-Over1MB.xml.b64"; //1.33 MB
+            config.Base64FileName = @"F:\Dev Tools\mongodbWin32_64.zip.b64.txt"; //138 MB
+
+            config.StringURL = "http://localhost:3001/ecrud/v1/core/electronicCaseFiles/transform";
+            //config.StringURL = "http://das.dynalias.org:8080/ecrud/v1/core/electronicCaseFiles/transform";
+            //for HTTPS
+            //config.StringURL = "https://goldvler.va.gov/ecrud/v1/core/electronicCaseFiles/transform";
+            //config.StringURL = "https://silvervler.va.gov/ecrud/v1/core/electronicCaseFiles/transform";
+            //config.StringURL = "https://silvervler.va.gov/ecrud/v1/core/electronicCaseFiles/transform?batchComplete=true&moroni=true";
+            //config.StringURL = "https://goldvler.va.gov/ecrud/v1/core/electronicCaseFiles/transform?batchComplete=true&moroni=true";
+            
+            config.DocTitle = "LargeAttachmentTitle-MoroniApp";
+            config.AttachmentDescription = "Mongodb zip file";
+            config.AttachmentFormatName = "application/zip";
+
+            config.PathToKey = @"E:\VA\2waySSL\clientcert.pkcs12";
+            config.KeyPassword = "keypass";
+            //POSTRequest.ClientCertificates.Add(new X509Certificate(@"F:\Downloads\ides-cftdeom.asmr.com.cer", "test123"));
+            //POSTRequest.ClientCertificates.Add(new X509Certificate(@"E:\VA\DoDCerts\dodRootCA2.cer"));
+            //POSTRequest.ClientCertificates.Add(new X509Certificate(@"E:\VA\DoDCerts\DODJITCCA-27.crt"));
+            //POSTRequest.ClientCertificates.Add(new X509Certificate(@"E:\VA\DoDCerts\ides-cftdemo.asmr.com.crt"));
+
+            config.ClientTimeout = 600000; //10 Min
+
+            //save config to file
+            return config;
+        }
+
+        public string DocTitle { get; set; }
+
+        public string AttachmentDescription { get; set; }
+
+        public string AttachmentFormatName { get; set; }
+
+        public string StringURL { get; set; }
+
+        public int PostTimes { get; set; }
+
+        public string PathToKey { get; set; }
+
+        public string KeyPassword { get; set; }
+
+        public int ClientTimeout { get; set; }
+
+        internal string getAttachmentFileName() {
+            return Path.GetFileName(this.Base64FileName);
+        }
+    }
+}
--- example/csharp.net/ConsoleHttpPost/Program.cs
+++ example/csharp.net/ConsoleHttpPost/Program.cs
@@ -6,88 +6,108 @@ using System.Net;
 using System.IO;
 using System.Globalization;
 using System.Security.Cryptography.X509Certificates;
+using System.Diagnostics;
 
 namespace ConsoleHttpPost {
     class Program {
         static void Main(string[] args) {
-            //string loadFileName = @"E:\VA\VLERDoc.xml";
-            //string loadFileName = @"E:\Code\GitHub\vlerdas-ecrud\test\attachments\eCFT1MBAttachEmbeded.xml";
-            //string loadFileName = @"C:\vler-proto-out\generated_out\xmlWithEmbeddedB64_5MB_a.xml"; //6.8 MB
-            //string loadFileName = @"C:\vler-proto-out\generated_out\xmlWithEmbeddedB64_14MB_a.xml"; //19.1 MB
-            //string loadFileName = @"C:\vler-proto-out\generated_out\xmlWithEmbeddedB64_17MB_a.xml"; //23.2 MB
-            string loadFileName = @"C:\vler-proto-out\generated_out\xmlWithEmbeddedB64_21MB_a.xml"; //28.7 MB
-            //string loadFileName = @"C:\vler-proto-out\generated_out\xmlWithEmbeddedB64_25MB_a.xml"; //34.2 MB
-            byte[] xmlData = System.Text.Encoding.UTF8.GetBytes(File.ReadAllText(loadFileName, Encoding.UTF8));
-            //string strURL = "http://das.dynalias.org:8080/ecrud/v1/core/electronicCaseFiles/transform";
-            //string strURL = "http://localhost:3001/ecrud/v1/core/electronicCaseFiles/transform";
-            //for HTTPS
-            //string strURL = "https://goldvler.va.gov/ecrud/v1/core/electronicCaseFiles/transform";
-            //string strURL = "https://silvervler.va.gov/ecrud/v1/core/electronicCaseFiles/transform";
-            //strURL += "?batchComplete=true";
-            //string strURL = "https://silvervler.va.gov/ecrud/v1/core/electronicCaseFiles/transform?batchComplete=true&moroni=true";
-            string strURL = "https://goldvler.va.gov/ecrud/v1/core/electronicCaseFiles/transform?batchComplete=true&moroni=true";
-            try {
-                HttpWebRequest POSTRequest = (HttpWebRequest)WebRequest.Create(strURL);
-                //For HTTPS two way cert, must be imported to "Trusted Root Certificate Authorities", Location: IE > Tools > Internet Options > Content > Certificates
-                POSTRequest.ClientCertificates.Add(new X509Certificate(@"E:\VA\2waySSL\clientcert.pkcs12", "keypass")); // ours/CACI
 
-
-                //POSTRequest.ClientCertificates.Add(new X509Certificate(@"F:\Downloads\ides-cftdeom.asmr.com.cer", "test123"));
-                //POSTRequest.ClientCertificates.Add(new X509Certificate(@"E:\VA\DoDCerts\dodRootCA2.cer"));
-                //POSTRequest.ClientCertificates.Add(new X509Certificate(@"E:\VA\DoDCerts\DODJITCCA-27.crt"));
-                //POSTRequest.ClientCertificates.Add(new X509Certificate(@"E:\VA\DoDCerts\ides-cftdemo.asmr.com.crt"));
-                //End HTTPS
-                POSTRequest.Method = "POST";
-                POSTRequest.KeepAlive = false;
-                POSTRequest.Timeout = 600000;
-                //Content length of message body
-                POSTRequest.Accept = "application/xml";
-
-                var boundary = "---------------------------" + DateTime.Now.Ticks.ToString("x", NumberFormatInfo.InvariantInfo);
-                POSTRequest.ContentType = "multipart/form-data; boundary=" + boundary;
-                boundary = "--" + boundary;
-
-                using (var requestStream = POSTRequest.GetRequestStream()) {
-                    var buffer = Encoding.ASCII.GetBytes(boundary + Environment.NewLine);
-                    requestStream.Write(buffer, 0, buffer.Length);
-                    string fileName = "some_fileName.xml";
-                    buffer = Encoding.UTF8.GetBytes(string.Format("Content-Disposition: form-data; name=\"{0}\"; filename=\"{1}\"{2}", "file", fileName, Environment.NewLine));
-                    requestStream.Write(buffer, 0, buffer.Length);
-                    buffer = Encoding.ASCII.GetBytes(string.Format("Content-Type: {0}{1}{1}", "text/xml", Environment.NewLine));
-                    requestStream.Write(buffer, 0, buffer.Length);
-
-                    requestStream.Write(xmlData, 0, xmlData.Length);
-
-                    buffer = Encoding.ASCII.GetBytes(Environment.NewLine);
-                    requestStream.Write(buffer, 0, buffer.Length);
-
-                    var boundaryBuffer = Encoding.ASCII.GetBytes(boundary + "--");
-                    requestStream.Write(boundaryBuffer, 0, boundaryBuffer.Length);
-                }
-
-                HttpWebResponse POSTResponse = (HttpWebResponse)POSTRequest.GetResponse();
-                StreamReader reader = new StreamReader(POSTResponse.GetResponseStream(), Encoding.UTF8);
-                string daLocation = POSTResponse.GetResponseHeader("Location");
-                HttpStatusCode daStatusCode = POSTResponse.StatusCode;
-
-                string ResponseFromPost = reader.ReadToEnd().ToString();
-                Console.WriteLine("StatusCode: " + daStatusCode);
-                System.Diagnostics.Debug.WriteLine("StatusCode: " + daStatusCode);
-
-                if (daStatusCode == HttpStatusCode.Accepted ||
-                    daStatusCode == HttpStatusCode.Created ||
-                    daStatusCode == HttpStatusCode.OK) {
-                    Console.Write(ResponseFromPost);
-                    System.Diagnostics.Debug.WriteLine(ResponseFromPost);
-                }
-            } catch (WebException we) {
-                String errMsg = we.Message;
-                if (we.Response != null)
-                    errMsg += "; " + new StreamReader(we.Response.GetResponseStream(), Encoding.UTF8).ReadToEnd().ToString();
-                System.Diagnostics.Debug.WriteLine(errMsg);
-                Console.Write(errMsg);
-            }
+            ConfigPOST config = ConfigPOST.getConfigPOST();
+            NiemXML niemXML = new NiemXML();
+            StringBuilder sb = new StringBuilder();
+            sb.Append(niemXML.getBeforeBase64(config.DocTitle));
+            string attachmentStr = File.ReadAllText(config.Base64FileName, Encoding.UTF8);
+            sb.Append(attachmentStr);
+            sb.Append(niemXML.getAfterBase64(attachmentStr.Length, config.AttachmentDescription, 
+                                                config.AttachmentFormatName, config.getAttachmentFileName()));
+            attachmentStr = null;
+            byte[] xmlData = System.Text.Encoding.UTF8.GetBytes(sb.ToString());
+            if (args.Length > 0)
+                config.StringURL = args[0];
+            config.PostTimes = 50;
+            logFileName = "ConsolePOST_" + DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss") + ".log";
+            double byteLen = xmlData.Length;
+            printLine("POSTing " + Math.Round(byteLen / 1024 / 1024, 2) + " MB (total) to:");
+            printLine(config.StringURL + Environment.NewLine);
+            doMultiPartPOSTs(xmlData, config);
             Console.In.Read();
+        }
+
+        private static void doMultiPartPOSTs(byte[] xmlData, ConfigPOST config) {
+            int postCount = 0;
+            while (postCount < config.PostTimes) {
+                printLine(DateTime.Now.ToString());
+                Stopwatch stopWatch = new Stopwatch();
+                postCount++;
+                try {
+                    HttpWebRequest POSTRequest = (HttpWebRequest)WebRequest.Create(config.StringURL);
+                    //For HTTPS two way cert, must be imported to "Trusted Root Certificate Authorities", Location: IE > Tools > Internet Options > Content > Certificates
+                    POSTRequest.ClientCertificates.Add(new X509Certificate(config.PathToKey, config.KeyPassword)); // ours/CACI
+                    //End HTTPS
+                    POSTRequest.Method = "POST";
+                    POSTRequest.KeepAlive = false;
+                    POSTRequest.Timeout = config.ClientTimeout;
+                    //Content length of message body
+                    POSTRequest.Accept = "application/xml";
+
+                    var boundary = "---------------------------" + DateTime.Now.Ticks.ToString("x", NumberFormatInfo.InvariantInfo);
+                    POSTRequest.ContentType = "multipart/form-data; boundary=" + boundary;
+                    boundary = "--" + boundary;
+
+                    stopWatch.Start();
+                    using (var requestStream = POSTRequest.GetRequestStream()) {
+                        var buffer = Encoding.ASCII.GetBytes(boundary + Environment.NewLine);
+                        requestStream.Write(buffer, 0, buffer.Length);
+                        buffer = Encoding.UTF8.GetBytes(string.Format("Content-Disposition: form-data; name=\"{0}\"; filename=\"{1}\"{2}", "file", 
+                                                            config.getAttachmentFileName(), Environment.NewLine));
+                        requestStream.Write(buffer, 0, buffer.Length);
+                        buffer = Encoding.ASCII.GetBytes(string.Format("Content-Type: {0}{1}{1}", "text/xml", Environment.NewLine));
+                        requestStream.Write(buffer, 0, buffer.Length);
+
+                        requestStream.Write(xmlData, 0, xmlData.Length);
+
+                        buffer = Encoding.ASCII.GetBytes(Environment.NewLine);
+                        requestStream.Write(buffer, 0, buffer.Length);
+
+                        var boundaryBuffer = Encoding.ASCII.GetBytes(boundary + "--");
+                        requestStream.Write(boundaryBuffer, 0, boundaryBuffer.Length);
+                    }
+
+                    HttpWebResponse POSTResponse = (HttpWebResponse)POSTRequest.GetResponse();
+                    StreamReader reader = new StreamReader(POSTResponse.GetResponseStream(), Encoding.UTF8);
+                    string daLocation = POSTResponse.GetResponseHeader("Location");
+                    HttpStatusCode daStatusCode = POSTResponse.StatusCode;
+
+                    string ResponseFromPost = reader.ReadToEnd().ToString();
+                    printLine("StatusCode: " + daStatusCode);
+
+                    if (daStatusCode == HttpStatusCode.Accepted ||
+                        daStatusCode == HttpStatusCode.Created ||
+                        daStatusCode == HttpStatusCode.OK) {
+                        printLine(ResponseFromPost);
+                    }
+                } catch (WebException we) {
+                    String errMsg = we.Message;
+                    if (we.Response != null)
+                        errMsg += "; " + new StreamReader(we.Response.GetResponseStream(), Encoding.UTF8).ReadToEnd().ToString();
+                    printLine(errMsg);
+                }
+                stopWatch.Stop();
+                TimeSpan ts = stopWatch.Elapsed;
+                string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                                                        ts.Hours, ts.Minutes, ts.Seconds,
+                                                        ts.Milliseconds / 10);
+                printLine("Elapsed Time: " + elapsedTime);
+                printLine("POST Count: " + postCount + Environment.NewLine);
+            }
+        }
+
+        static string logFileName = "ConsolePOST.log";
+
+        private static void printLine(String msg) {
+            System.Diagnostics.Debug.WriteLine(msg);
+            Console.WriteLine(msg);
+            File.AppendAllText(logFileName, msg + Environment.NewLine);
         }
     }
 }
